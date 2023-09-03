3af28708146e85e2b4fc56e349a27f0ec96af171
Peter Bosák <bosakpet@gmail.com>
2023-09-03 09:44:23 +0200

    Initialize the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+
+        return 0;
+    }
+}
